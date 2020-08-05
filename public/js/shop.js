f0d435a7b54d5bed481f1d9ef725b039aaabd13c
Jherico Cocosa <njcocosa@gmail.com>
2020-08-06 04:08:46 +0800

    new

--- public/js/shop.js
+++ public/js/shop.js
@@ -25,13 +25,54 @@ function ready() {
     document.getElementsByClassName('order')[0].addEventListener('click', purchaseClicked)
 }
 
-function purchaseClicked() {
-    alert('Thank you for your purchase')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    while (cartItems.hasChildNodes()) {
-        cartItems.removeChild(cartItems.firstChild)
+var stripeHandler = StripeCheckout.configure({
+    key: stripePublicKey,
+    locale: 'auto',
+    token: function (token) {
+        var items = []
+        var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+        var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+        for (var i = 0; i < cartRows.length; i++) {
+            var cartRow = cartRows[i]
+            var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+            var quantity = quantityElement.value
+            var id = cartRow.dataset.itemId
+            items.push({
+                id: id,
+                quantity: quantity
+            })
+        }
+        fetch('/purchase', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                stripeTokenId: token.id,
+                items: items
+            })
+        }).then(function (res) {
+            return res.json()
+        }).then(function (data) {
+            alert(data.message)
+            var cartItems = document.getElementsByClassName('cart-items')[0]
+            while (cartItems.hasChildNodes()) {
+                cartItems.removeChild(cartItems.firstChild)
+            }
+            updateCartTotal()
+        }).catch(function (error) {
+            console.log(error)
+        })
     }
-    updateCartTotal()
+})
+
+function purchaseClicked() {
+    var priceElement = document.getElementsByClassName('cart-total-price')[0]
+    var price = parseFloat(priceElement.innerHTML.replace('₱', '')) * 100
+    stripeHandler.open({
+        amount: price
+    })
 }
 
 function removeCartItem(event) {
@@ -54,14 +95,16 @@ function addToCartClicked(event) {
     var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
     var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
     var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
-    addItemToCart(title, price, imageSrc)
+    var id = shopItem.dataset.itemId;
+    addItemToCart(title, price, imageSrc, id)
     console.log(title, price);
     updateCartTotal()
 }
 
-function addItemToCart(title, price, imageSrc) {
+function addItemToCart(title, price, imageSrc, id) {
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
+    cartRow.dataset.itemId = id
     var cartItems = document.getElementsByClassName('cart-items')[0]
     var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
     for (var i = 0; i < cartItemNames.length; i++) {
--- server.js
+++ server.js
@@ -1,13 +1,48 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY
-const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+const path = require('path')
 
-const express = require('express')
-const app = express();
+const server = http.createServer(function (req, res) {
+    if (req.url === "/") {                              //RUNNING THE index.html OR THE MAIN FILE
+        fs.readFile('index.html', function (error, data) {
+            res.writeHead(200, { 'Content-type': 'text/html' })
+            res.write(data);
+            res.end();
+        });
+    } else if (req.url.match("\.html$")) {               //GETTING THE HTML FILE
+        var html_file = path.join(__dirname, req.url);
+        var filestream = fs.createReadStream(html_file);
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        filestream.pipe(res)
+    }
+    else if (req.url.match("\.css$")) {                 //GETTING THE CSS FILE
+        var css_file = path.join(__dirname, req.url);
+        var filestream = fs.createReadStream(css_file);
+        res.writeHead(200, { 'Content-type': 'text/css' });
+        filestream.pipe(res)
+    } else if (req.url.match("\.js$")) {                 //GETTING THE JS FILE
+        var js_file = path.join(__dirname, req.url);
+        var filestream = fs.createReadStream(js_file);
+        res.writeHead(200, { 'Content-type': 'text/js' });
+        filestream.pipe(res)
+    } else if (req.url.match("\.jpg$")) {                 //GETTING THE PICTURE FILE
+        var pic_file = path.join(__dirname, req.url);
+        var filestream = fs.createReadStream(pic_file);
+        res.writeHead(200, { 'Content-type': 'image/jpg' });
+        filestream.pipe(res)
+    } else {
+        res.writeHead(404)
+        res.write("Error file")
+        res.end()
+    }
+})
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.listen(3000);
-console.log('SUCCESS')
+server.listen(port, function (error) {
+    if (error) {
+        console.log("Error", error)
+    } else {
+        console.log("Success", +port)
+    }
+})
+
